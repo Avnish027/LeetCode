2144525633351ee408ea5a07789c5e4b281098df
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-04-02 11:04:31 +0530

    Time: 388 ms (5.08%), Space: 22.3 MB (96.60%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int i=0; 
+        sort(intervals.begin(), intervals.end());
+        while(i<intervals.size()-1){
+            if(intervals[i][1]>=intervals[i+1][0]){
+                if(intervals[i][1] <= intervals[i+1][1]){
+                    intervals[i][1]=intervals[i+1][1];
+                intervals.erase(intervals.begin()+i+1);
+                }
+                else{
+                intervals.erase(intervals.begin()+i+1);
+
+                }
+            }
+            else i++;
+        }
+        return intervals;
+        
+    }
+};
