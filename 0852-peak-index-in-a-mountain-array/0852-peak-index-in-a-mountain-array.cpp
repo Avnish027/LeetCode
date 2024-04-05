aef0178583061e9fce424b76fbfd88f63706f196
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-04-05 16:50:16 +0530

    Time: 83 ms (51.01%), Space: 61.9 MB (98.47%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int s=0;
-        int e=arr.size()-1;
-        int mid=s+(e-s)/2;
-        while(s<e){
-            if(arr[mid]<arr[mid+1]) s=mid+1;
-            else e=mid;
-            mid=s+(e-s)/2;
-        }
-        return mid;
-        
-        // int s=0, e=arr.size()-1;int mid=s+(e-s)/2;
-        // while(s<=e){
-        //     if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]) return mid;
-        //     else if(arr[mid]>arr[mid+1]) e=mid-1;
-        //     else s=mid+1;
+        // int s=0;
+        // int e=arr.size()-1;
+        // int mid=s+(e-s)/2;
+        // while(s<e){
+        //     if(arr[mid]<arr[mid+1]) s=mid+1;
+        //     else e=mid;
         //     mid=s+(e-s)/2;
         // }
-        // return -1;
+        // return mid;
+        
+        int s=0, e=arr.size()-1;int mid=s+(e-s)/2;
+        while(s<=e){
+            if(mid-1>=0 && arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]) return mid;
+            else if(arr[mid]>arr[mid+1]) e=mid-1;
+            else s=mid+1;
+            mid=s+(e-s)/2;
+        }
+        return arr[mid];
     }
 };
