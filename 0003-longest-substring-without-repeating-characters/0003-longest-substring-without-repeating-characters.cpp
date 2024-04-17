726e5dc5d68087fbdd875f3f9d1959c9f0e85375
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-04-17 10:57:23 +0530

    Time: 6 ms (85.72%), Space: 10.4 MB (75.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -13,14 +13,14 @@ public:
         // return max_len;
         
         
-        int max_len=0;
-        for(int i=0; i<s.size(); i++){
-            vector<int> v(257,0);
-            for(int j=i; j<s.size(); j++){
-                if(v[s[j]]!=0) break;
-                max_len=max(max_len,j-i+1);
-                v[s[j]]++;
+       int max_len=0, l=0,e=0;
+        vector<int> v(257,0);
+        while(e<s.size()){
+            while(v[s[e]]!=0){
+                v[s[l++]]=0;
             }
+            max_len=max(max_len,e-l+1);
+            v[s[e++]]=1;
         }
         return max_len;
     }
