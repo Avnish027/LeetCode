6ad992e30a970ca59003c1a2152b1b71f12ecdc4
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-04-20 21:47:25 +0530

    Time: 4 ms (38.70%), Space: 8.7 MB (21.67%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int rotate= matrix.size()-1;
-        vector<vector<int>> v(matrix.size(),vector<int>(matrix.size()));
-        for(int i=0; i<matrix.size(); i++){
-            for(int j=0; j<matrix.size(); j++){
-                v[j][rotate]=matrix[i][j];
-            }
-            rotate--;
-        }
+//         int rotate= matrix.size()-1;
+//         vector<vector<int>> v(matrix.size(),vector<int>(matrix.size()));
+//         for(int i=0; i<matrix.size(); i++){
+//             for(int j=0; j<matrix.size(); j++){
+//                 v[j][rotate]=matrix[i][j];
+//             }
+//             rotate--;
+//         }
         
-        for(int i=0; i<matrix.size(); i++){
-            for(int j=0; j<matrix.size(); j++){
-                matrix[i][j]=v[i][j];
+//         for(int i=0; i<matrix.size(); i++){
+//             for(int j=0; j<matrix.size(); j++){
+//                 matrix[i][j]=v[i][j];
+//             }
+//         }
+       int row=matrix.size();
+        int col= matrix[0].size();
+        for(int i=0; i<row; i++){
+            for(int j=0; j<i; j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        int a=col/2;
+        for(int i=0; i<a; i++){
+            for(int j=0; j<row; j++){
+                swap(matrix[j][i],matrix[j][col-1-i]);
             }
         }
     }
