46a9f30c615a077822bd794f9fc09bd271b766b5
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-04-21 21:50:08 +0530

    Time: 3 ms (67.74%), Space: 12 MB (49.74%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -2,37 +2,50 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        int s1=0;
-        int e1=matrix.size()-1;
-        int mid=s1+(e1-s1)/2;
-        int ans=-1;
-        while(s1<=e1){
-            if(matrix[mid][0]==target){
-            return true;
-            }
-            else if(matrix[mid][0]>target){
-            e1=mid-1;
-            }
-            else{
-            ans=mid;
-            s1=mid+1;
-            }
-            mid=s1+(e1-s1)/2;
+//         int s1=0;
+//         int e1=matrix.size()-1;
+//         int mid=s1+(e1-s1)/2;
+//         int ans=-1;
+//         while(s1<=e1){
+//             if(matrix[mid][0]==target){
+//             return true;
+//             }
+//             else if(matrix[mid][0]>target){
+//             e1=mid-1;
+//             }
+//             else{
+//             ans=mid;
+//             s1=mid+1;
+//             }
+//             mid=s1+(e1-s1)/2;
+//         }
+//     if(ans==-1){
+//         return 0;
+//     }
+        
+        
+//     int s2=0;
+//     int e2= matrix[ans].size()-1;
+//     int mid2=s2 +(e2-s2)/2;
+//     while(s2<=e2){
+//     if(matrix[ans][mid2]==target) return true;
+//         else if(matrix[ans][mid2]>target) e2=mid2-1;
+//         else s2=mid2+1;
+//         mid2=s2+(e2-s2)/2;
+//     }
+//         return false;
+        
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int s=0, e=row*col-1;
+        int mid=(s+e)/2;
+        while(s<=e){
+            if(matrix[mid/col][mid%col]==target) return true;
+            else if(matrix[mid/col][mid%col]>target)e=mid-1;
+            else s=mid+1;
+            mid=(s+e)/2;
         }
-    if(ans==-1){
-        return 0;
-    }
+        return false;         
         
-        
-    int s2=0;
-    int e2= matrix[ans].size()-1;
-    int mid2=s2 +(e2-s2)/2;
-    while(s2<=e2){
-    if(matrix[ans][mid2]==target) return true;
-        else if(matrix[ans][mid2]>target) e2=mid2-1;
-        else s2=mid2+1;
-        mid2=s2+(e2-s2)/2;
-    }
-        return false;
     }
 };
