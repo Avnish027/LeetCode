6f6bb2129e7f4a024d05093971987e74f37a0617
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-04-23 08:21:40 +0530

    Time: 1404 ms (5.02%), Space: 36.7 MB (5.01%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        //Brute Force approach
+        // int max_len=0;
+        // for(int i=0; i<s.size(); i++){
+        //     vector<int> v(26,0); int maxi=INT_MIN;
+        //     for(int j=i; j<s.size(); j++){
+        //         v[s[j]-'A']++;
+        //         maxi=max(maxi,v[s[j]-'A']);
+        //         int diff= j-i+1-maxi;
+        //         if(diff<=k) max_len=max(max_len,j-i+1);
+        //         else break;
+        //     }
+        // }
+        // return max_len;
+        
         int max_len=0;
         for(int i=0; i<s.size(); i++){
-            vector<int> v(26,0); int maxi=INT_MIN;
+            vector<int> v(26,0); int max_freq=0;
             for(int j=i; j<s.size(); j++){
                 v[s[j]-'A']++;
-                maxi=max(maxi,v[s[j]-'A']);
-                int diff= j-i+1-maxi;
-                if(diff<=k) max_len=max(max_len,j-i+1);
+                max_freq=max(max_freq, v[s[j]-'A']);
+                if(j-i+1-max_freq<=k) max_len=max(max_len,j-i+1);
                 else break;
             }
         }
