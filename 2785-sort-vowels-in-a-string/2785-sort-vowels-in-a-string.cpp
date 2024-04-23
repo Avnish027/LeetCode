b146d5c57ee6626bc854b08be0561cf46352419f
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-04-23 11:34:01 +0530

    Time: 42 ms (27.75%), Space: 14.7 MB (84.27%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        vector<int> large(26,0);
-        vector<int> small(26,0);
-        for(int i=0; i<s.size(); i++){
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'  ){
-                small[s[i]-'a']++;
-                s[i]='#';
-            }
-             else if(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'  ){
-                large[s[i]-'A']++;
-                s[i]='#';
-            } 
-        }
-        string vow="";
-        for(int i=0; i<large.size(); i++){
-            while(large[i]--){
-            vow+=(i+'A');
-            }}
-            for(int i=0; i<small.size(); i++){
-            while(small[i]--){
-            vow+=(i+'a');
-            }
-        }    
+//         vector<int> large(26,0);
+//         vector<int> small(26,0);
+//         for(int i=0; i<s.size(); i++){
+//             if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'  ){
+//                 small[s[i]-'a']++;
+//                 s[i]='#';
+//             }
+//              else if(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'  ){
+//                 large[s[i]-'A']++;
+//                 s[i]='#';
+//             } 
+//         }
+//         string vow="";
+//         for(int i=0; i<large.size(); i++){
+//             while(large[i]--){
+//             vow+=(i+'A');
+//             }}
+//             for(int i=0; i<small.size(); i++){
+//             while(small[i]--){
+//             vow+=(i+'a');
+//             }
+//         }    
         
-        int k=0;
-        for(int i=0; i<s.size();i++){
-            if(s[i]=='#') s[i]=vow[k++];
+//         int k=0;
+//         for(int i=0; i<s.size();i++){
+//             if(s[i]=='#') s[i]=vow[k++];
+//         }
+//         return s;
+        
+        string vowel="";
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' ||s[i]=='I' ||s[i]=='O' ||s[i]=='U' ) vowel.push_back(s[i]);
+        }
+        sort(vowel.begin(), vowel.end());
+        int j=0;
+         for(int i=0; i<s.size(); i++){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' ||s[i]=='I' ||s[i]=='O' ||s[i]=='U' ) s[i]=vowel[j++];
         }
         return s;
     }
