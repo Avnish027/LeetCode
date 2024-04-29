520fc4252b8f6d8b9e423cbbf42148003cb73d63
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-04-29 16:18:56 +0530

    Time: 8 ms (48.72%), Space: 15.3 MB (19.32%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -11,26 +11,50 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(head==NULL) return NULL;
-        set<int> v;
-        ListNode* temp= head;
-        while(temp){
-            v.insert(temp->val);
-            temp=temp->next;
-        }
-        temp=head;
-        for(auto it=v.begin(); it!=v.end(); it++){
-            temp->val=*it;
-            temp=temp->next;
-        }
-        int size=v.size()-1;
-        temp=head;
-        while(size--){
-            temp=temp->next;
-        }
-        temp->next=NULL;
-        return head;
+        // Taking the extra space
+        // if(head==NULL) return NULL;
+        // set<int> v;
+        // ListNode* temp= head;
+        // while(temp){
+        //     v.insert(temp->val);
+        //     temp=temp->next;
+        // }
+        // temp=head;
+        // for(auto it=v.begin(); it!=v.end(); it++){
+        //     temp->val=*it;
+        //     temp=temp->next;
+        // }
+        // int size=v.size()-1;
+        // temp=head;
+        // while(size--){
+        //     temp=temp->next;
+        // }
+        // temp->next=NULL;
+        // return head;
         
+        
+        // Without taking the extra space
+        if(head==NULL) return NULL;
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+        while(curr){
+            if(prev!=NULL){
+                if(prev->val== curr->val){
+                    prev->next=curr->next;
+                    delete curr;
+                    curr=prev->next;
+                }
+                else{
+                    prev=curr;
+                    curr=curr->next;
+                }
+            }
+            else{
+                prev=curr;
+                curr=curr->next;
+            }
+        }
+        return head;
         
     }
 };
