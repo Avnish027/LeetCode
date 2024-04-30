9fc71c7106d30f90aba9f54d8a54f4b8239d9dee
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-04-30 09:46:46 +0530

    Time: 7 ms (72.71%), Space: 8.4 MB (51.98%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -15,35 +15,28 @@ public:
         // }
         // return max_len;
         
-        // int l=0, r=0, max_len=0, max_freq=0;
-        // vector<int> v(26,0);
-        // while(r<s.size()){
-        //     v[s[r]-'A']++;
-        //     max_freq=max(max_freq,v[s[r]-'A']);
-        //     while(r-l+1-max_freq>k){
-        //         v[s[l]-'A']--;
-        //         for(int i=0; i<26; i++) max_freq=max(max_freq,v[s[l]-'A']);
-        //         l++;
-        //     }
-        //     max_len=max(max_len,r-l+1);
-        //     r++;
-        // }
-        // return max_len;
-        
-        
-        
-        int max_len=0;
-        for(int i=0; i<s.size(); i++){
-            vector<int> v(26,0);int max_freq=0;
-            for(int j=i; j<s.size(); j++){
-                v[s[j]-'A']++;
-                max_freq=max(max_freq,v[s[j]-'A']);
-                if((j-i+1)-max_freq<=k) max_len=max(max_len,j-i+1);
-                else break;
+        int l=0, r=0, max_len=0, max_freq=0;
+        vector<int> v(26,0);
+        while(r<s.size()){
+            v[s[r]-'A']++;
+            max_freq=max(max_freq,v[s[r]-'A']);
+            while(r-l+1-max_freq>k){
+                v[s[l]-'A']--;
+                for(int i=0; i<26; i++) max_freq=max(max_freq,v[s[l]-'A']);
+                l++;
             }
+            max_len=max(max_len,r-l+1);
+            r++;
         }
         return max_len;
         
+        
+        
+        
+        
+        
+        
+       
         
         
         
