48dceee2f1b790adcc694743c5a005e3381780b3
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-05-16 21:30:47 +0530

    Time: 369 ms (65.20%), Space: 11.9 MB (13.28%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -6,23 +6,46 @@ public:
         return (i>=0)&&(i<row)&&(j>=0)&&(j<col);
     }
     
-    void fun(vector<vector<char>>& board, string word, int i, int j, int word_ind, bool& ans, vector<vector<bool>>& visited){
+//     void fun(vector<vector<char>>& board, string word, int i, int j, int word_ind, bool& ans, vector<vector<bool>>& visited){
+//         if(word_ind>=word.size()) {ans=true;
+//                                   return;}
+//         visited[i][j]=1;
+//         if(valid(board, i+1, j) && word[word_ind]==board[i+1][j] && !visited[i+1][j]){
+//             fun(board, word, i+1,j,word_ind+1, ans,visited);
+//         }
+//         if(valid(board, i-1, j) && word[word_ind]==board[i-1][j] && !visited[i-1][j]){
+//             fun(board, word, i-1,j,word_ind+1, ans,visited);
+//         }
+//         if(valid(board, i, j+1) && word[word_ind]==board[i][j+1] && !visited[i][j+1]){
+//             fun(board, word, i,j+1,word_ind+1, ans,visited);
+//         }
+//         if(valid(board, i, j-1) && word[word_ind]==board[i][j-1] && !visited[i][j-1]){
+//             fun(board, word, i,j-1,word_ind+1, ans,visited);
+//         }
+//         visited[i][j]=0;
+        
+//     }
+    
+    
+    
+    void fun(vector<vector<char>>& board, string word, int i, int j, int word_ind, bool& ans){
         if(word_ind>=word.size()) {ans=true;
                                   return;}
-        visited[i][j]=1;
-        if(valid(board, i+1, j) && word[word_ind]==board[i+1][j] && !visited[i+1][j]){
-            fun(board, word, i+1,j,word_ind+1, ans,visited);
+        char a= board[i][j];
+        board[i][j]=1;
+        if(valid(board, i+1, j) && word[word_ind]==board[i+1][j] && !board[i+1][j]!=1){
+            fun(board, word, i+1,j,word_ind+1, ans);
         }
-        if(valid(board, i-1, j) && word[word_ind]==board[i-1][j] && !visited[i-1][j]){
-            fun(board, word, i-1,j,word_ind+1, ans,visited);
+        if(valid(board, i-1, j) && word[word_ind]==board[i-1][j] && !board[i-1][j]!=1){
+            fun(board, word, i-1,j,word_ind+1, ans);
         }
-        if(valid(board, i, j+1) && word[word_ind]==board[i][j+1] && !visited[i][j+1]){
-            fun(board, word, i,j+1,word_ind+1, ans,visited);
+        if(valid(board, i, j+1) && word[word_ind]==board[i][j+1] && !board[i][j+1]!=1){
+            fun(board, word, i,j+1,word_ind+1, ans);
         }
-        if(valid(board, i, j-1) && word[word_ind]==board[i][j-1] && !visited[i][j-1]){
-            fun(board, word, i,j-1,word_ind+1, ans,visited);
+        if(valid(board, i, j-1) && word[word_ind]==board[i][j-1] && board[i][j-1]!=1){
+            fun(board, word, i,j-1,word_ind+1, ans);
         }
-        visited[i][j]=0;
+        board[i][j]=a;
         
     }
     
@@ -35,7 +58,7 @@ public:
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
                 if(board[i][j]==word[0]){
-                    fun(board, word, i ,j,1,ans,visited);
+                    fun(board, word, i ,j,1,ans);
                 }
             }
         }
