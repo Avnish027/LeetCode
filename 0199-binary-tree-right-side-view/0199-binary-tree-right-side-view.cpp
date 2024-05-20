341a140cf3c6fa53c166a018543aa2ac1da620dc
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-05-20 17:38:47 +0530

    Time: 3 ms (57.84%), Space: 13.8 MB (98.02%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,24 +11,41 @@
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
-        queue<TreeNode*> q;
-        
-        q.push(root);
-        vector<int> ans;
-                if(!root) return ans;
+    int fun(TreeNode* root){
+        if(!root) return 0;
+        return 1+max(fun(root->left), fun(root->right));
+    }
+    
+    void right_view(TreeNode* root, int level, vector<int>& v){
+        if(!root) return;
+        if(v[level]==-101) v[level]=root->val;
+        right_view(root->right, level+1, v);
+        right_view(root->left, level+1, v);
 
-        while(!q.empty()){
-            int n=q.size();
-            TreeNode* alpha=q.front();
-            ans.push_back(q.front()->val);
-            while(n--){
-                TreeNode* temp=q.front();
-                q.pop();
-                if(temp->right) q.push(temp->right);
-                if(temp->left) q.push(temp->left);
-            }
-        }
-        return ans;
+    }
+    vector<int> rightSideView(TreeNode* root) {
+//         //Iterative approach
+//         queue<TreeNode*> q;
+//         q.push(root);
+//         vector<int> ans;
+//         if(!root) return ans;
+
+//         while(!q.empty()){
+//             int n=q.size();
+//             TreeNode* alpha=q.front();
+//             ans.push_back(q.front()->val);
+//             while(n--){
+//                 TreeNode* temp=q.front();
+//                 q.pop();
+//                 if(temp->right) q.push(temp->right);
+//                 if(temp->left) q.push(temp->left);
+//             }
+//         }
+//         return ans;
+        
+        int height=fun(root);
+                vector<int> v(height,-101);
+        right_view(root,0,v);
+        return v;
     }
 };
