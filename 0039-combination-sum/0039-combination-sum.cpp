74e806e5c92557943ac65b9b4129c484d47492f5
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-05-27 13:42:30 +0530

    Time: 26 ms (23.79%), Space: 22.9 MB (21.58%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
-    void fun(vector<int>& candi, int i, int target, vector<int>& v, vector<vector<int>>& ans){
-        if(i>=candi.size() || target<0) return;
-        if(target==0) {
-            ans.push_back(v);
-        }
-        for(int j=i; j<candi.size(); j++){
-            v.push_back(candi[j]);
-            fun(candi,j,target-candi[j],v,ans);
-            v.pop_back();
-        }
-    }
+    // void fun(vector<int>& candi, int i, int target, vector<int>& v, vector<vector<int>>& ans){
+    //     if(i>=candi.size() || target<0) return;
+    //     if(target==0) {
+    //         ans.push_back(v);
+    //     }
+    //     for(int j=i; j<candi.size(); j++){
+    //         v.push_back(candi[j]);
+    //         fun(candi,j,target-candi[j],v,ans);
+    //         v.pop_back();
+    //     }
+    // }
     
+    void fun(vector<int> v, int i, int target, vector<int>& ds, vector<vector<int>>& ans){
+        if(i>=v.size() || target<0) return ;
+        if(target==0){
+            ans.push_back(ds);
+            return;
+        }
+        for(int j=i; j<v.size(); j++){
+            ds.push_back(v[j]);
+            fun(v,j,target-v[j],ds, ans);
+            ds.pop_back();
+        }
+    
+    }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
        vector<int> v;
