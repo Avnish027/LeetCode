29709e89c2ee41b7309c33c5b5eb69bfeeaa37f1
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-05-27 12:00:06 +0530

    Time: 0 ms (100.00%), Space: 8 MB (44.26%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,26 @@
 class Solution {
 public:
+    
+    // double fun(double x, int n){
+    //     if(n==0) return 1;
+    //     if(n%2){
+    //         return x*fun(x,n/2)*fun(x,n/2);
+    //     }
+    //     else return fun(x,n/2)*fun(x,n/2);
+    // }
+    
+    double fun(double x, int n){
+        if(n==1) return x;
+        if(n==0) return 1;
+        if(n%2==0) return fun(x*x,n/2);
+        else return x*fun(x*x,n/2);
+    }
+    
     double myPow(double x, int n) {
-        double ans=pow(x,n);
-        return ans;
+        // double ans=pow(x,n);
+        // return ans;
+        double ans=fun(x,abs(n));
+        if(n<0) return 1/ans;
+        else return ans;
     }
 };
