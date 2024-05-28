de97ab417a1deab30b41ea0676e23efed5693875
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-05-28 09:20:35 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (23.91%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -19,38 +19,22 @@ public:
 //     }
     
     // //Optimizing the space
-    // void fun(int i, vector<int> v, vector<vector<int>>& ans){
-    //     if(i>=v.size()){ ans.push_back(v);return;}
-    //     for(int j=i; j<v.size(); j++){
-    //         swap(v[i],v[j]);
-    //         fun(i+1,v,ans);
-    //         swap(v[i],v[j]);
-    //     }
-    // }
-    
-    void fun(vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans){
-        if(ds.size()==nums.size()) {
-            ans.push_back(ds);
-            return;
-        }
-        for(int i=0;i<nums.size(); i++){
-            if(nums[i]!=-11){
-                int temp=nums[i];
-                nums[i]=-11;
-                ds.push_back(temp);
-                fun(nums,ds,ans);
-                nums[i]=temp;
-                ds.pop_back();
-            }
+    void fun(int i, vector<int> v, vector<vector<int>>& ans){
+        if(i>=v.size()){ ans.push_back(v);return;}
+        for(int j=i; j<v.size(); j++){
+            swap(v[i],v[j]);
+            fun(i+1,v,ans);
+            swap(v[i],v[j]);
         }
     }
+    
     
     
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> ds;
-        fun(nums,ds,ans);
-        // fun(0,nums,ans);
+        // fun(nums,ds,ans);
+        fun(0,nums,ans);
         return ans;
     }
 };
