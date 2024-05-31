907205bb4c3b66b10ab8bf879eace4c0d24a9660
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-05-31 10:38:07 +0530

    Time: 0 ms (100.00%), Space: 13.6 MB (10.26%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int len=nums.size();
-        sort(nums.begin(), nums.end());
-        if(len==2 || len==1) return nums[0];
-        for(int i=0; i<len; i+=3){
-            if(i==len-1) return nums[len-1];
-            if(nums[i]!=nums[i+1]) return nums[i];
-            if(nums[i+1]!=nums[i+2]) return nums[i+1];
+        // int len=nums.size();
+        // sort(nums.begin(), nums.end());
+        // if(len==2 || len==1) return nums[0];
+        // for(int i=0; i<len; i+=3){
+        //     if(i==len-1) return nums[len-1];
+        //     if(nums[i]!=nums[i+1]) return nums[i];
+        //     if(nums[i+1]!=nums[i+2]) return nums[i+1];
+        // }
+        // return 0;
+        unordered_map<int,int> m;
+        for(int i=0; i<nums.size(); i++){
+            m[nums[i]]++;
+        }
+        for(auto it:m){
+            if(it.second==1) return it.first;
         }
         return 0;
     }
