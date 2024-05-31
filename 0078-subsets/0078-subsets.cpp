daa254786ee900433d246155897e31a40b12b1d7
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-05-31 10:13:15 +0530

    Time: 2 ms (51.80%), Space: 8.3 MB (58.35%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
-    void fun(vector<int> &nums, int i, vector<vector<int>> &v, vector<int> &temp){
-        if(i>=nums.size()){
-            v.push_back(temp);return;
-        }
-        fun(nums,i+1,v,temp);
-        temp.push_back(nums[i]);
-        fun(nums,i+1,v,temp);
-        temp.pop_back();
-    }
+    // void fun(vector<int> &nums, int i, vector<vector<int>> &v, vector<int> &temp){
+    //     if(i>=nums.size()){
+    //         v.push_back(temp);return;
+    //     }
+    //     fun(nums,i+1,v,temp);
+    //     temp.push_back(nums[i]);
+    //     fun(nums,i+1,v,temp);
+    //     temp.pop_back();
+    // }
     
     vector<vector<int>> subsets(vector<int>& nums) {
-        int i=0;
-        vector<int> temp;
-        vector<vector<int>> v;
-        fun(nums,i,v,temp);
-        return v;
+        // int i=0;
+        // vector<int> temp;
+        // vector<vector<int>> v;
+        // fun(nums,i,v,temp);
+        // return v;
         
+        int len=nums.size();
+        int total=pow(2,len);
+        vector<vector<int>> ans;
+        for(int i=0; i<total; i++){
+            vector<int> v;
+            int temp=i;int count=0;
+            while(temp){
+                if(temp&1) v.push_back(nums[count]);
+                temp=temp>>1;
+                count++;
+            }
+            ans.push_back(v);
+        }
+        
+        return ans;
     }
 };
