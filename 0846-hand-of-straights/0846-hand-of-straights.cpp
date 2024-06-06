4ab1ba0c5fa24ca6af91bc1cd49d02a0b52ee1f4
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-06-06 14:19:59 +0530

    Time: 63 ms (47.40%), Space: 30.8 MB (42.24%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if((hand.size())%groupSize) return false;
+       map<int,int> mp;
+        for(int i=0; i<hand.size(); i++){
+            mp[hand[i]]++;
+        }
+        // int group= hand.size()/groupSize;
+        // for(int i=0; i<group; i++){
+        //     auto start= mp.begin();
+        //     int start_ele= start->first;
+        //     int j=1;
+        //     start->second--;
+        //     if(start->second==0) mp.erase(start_ele);
+        //     int w=groupSize-1;
+        //     while((w)--){
+        //         if((++start)->first==start_ele+1){
+        //             j++; 
+        //             continue;
+        //         }
+        //         else return false;
+        //     }
+        // }
+        // return true;
+        int groupCount = hand.size() / groupSize;
+        for (int i = 0; i < groupCount; i++) {
+            // Get the iterator to the smallest element in the map
+            auto start = mp.begin();
+            int start_ele = start->first;
+            
+            // Decrement the count of the starting element
+            start->second--;
+            if (start->second == 0) mp.erase(start);
+            
+            // Check the next `groupSize - 1` elements
+            for (int j = 1; j < groupSize; j++) {
+                // Increment the iterator
+                start = mp.find(start_ele + j);
+                // If the next consecutive element is not found, return false
+                if (start == mp.end()) return false;
+                // Decrement the count of the current element
+                start->second--;
+                // If the count becomes zero, erase the element from the map
+                if (start->second == 0) mp.erase(start);
+            }
+        }
+        return true;
+
+    }
+};
