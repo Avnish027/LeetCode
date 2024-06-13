91abe15c8edced3144f8d01eaf06109e4cd7d261
Avnish027 <141852943+Avnish027@users.noreply.github.com>
2024-06-13 22:53:19 +0530

    Time: 50 ms (72.82%), Space: 30.7 MB (42.87%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -25,29 +25,7 @@ public:
             }
         }
         return true;
-//         int groupCount = hand.size() / groupSize;
-//         for (int i = 0; i < groupCount; i++) {
-//             // Get the iterator to the smallest element in the map
-//             auto start = mp.begin();
-//             int start_ele = start->first;
-            
-//             // Decrement the count of the starting element
-//             start->second--;
-//             if (start->second == 0) mp.erase(start);
-            
-//             // Check the next `groupSize - 1` elements
-//             for (int j = 1; j < groupSize; j++) {
-//                 // Increment the iterator
-//                 start = mp.find(start_ele + j);
-//                 // If the next consecutive element is not found, return false
-//                 if (start == mp.end()) return false;
-//                 // Decrement the count of the current element
-//                 start->second--;
-//                 // If the count becomes zero, erase the element from the map
-//                 if (start->second == 0) mp.erase(start);
-//             }
-//         }
-//         return true;
+
 
     }
 };
